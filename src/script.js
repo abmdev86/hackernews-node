55290194ba28a416dfd1d598cf5dccf97542e58f
Auroiah Morgan <amwebdev@outlook.com>
2022-01-18 17:16:26 -0500

    adds nodemon

--- src/script.js
+++ src/script.js
@@ -4,12 +4,12 @@ const prisma = new PrismaClient();
 
 async function main() {
   const allLinks = await prisma.link.findMany();
-  const newLink = await prisma.link.create({
-    data: {
-      description: "Fullstack tutorial for GraphQL",
-      url: 'www.howtographql.com',
-    }
-  });
+  // const newLink = await prisma.link.create({
+  //   data: {
+  //     description: "Fullstack tutorial for GraphQL",
+  //     url: 'www.howtographql.com',
+  //   }
+  // });
   console.log(allLinks);
 }
 main().catch(e => {
